d36ee62e8c831f8394aa9e44c19753aff8184c83
Akshay Chandrashekaran <akshayc@cmu.edu>
2016-12-29 16:57:38 -0800

    feat: added validation check to simple. For now it just returns the job id.
    chore: lint checked the branin.py

--- examples/simple/branin.py
+++ examples/simple/branin.py
@@ -1,24 +1,30 @@
-import numpy as np
 import math
 import time
+
+import numpy as np
+
+
 def branin(x, y):
 
+    result = np.square(y - (5.1 / (4 * np.square(math.pi))) * np.square(x) +
+                       (5 / math.pi) * x - 6) + 10 * (1 - (1. / (8 * math.pi))
+                                                      ) * np.cos(x) + 10
 
-    result = np.square(y - (5.1/(4*np.square(math.pi)))*np.square(x) + 
-         (5/math.pi)*x - 6) + 10*(1-(1./(8*math.pi)))*np.cos(x) + 10
-    
     result = float(result)
-    
+
     print 'Result = %f' % result
-    #time.sleep(np.random.randint(60))
+
     return result
+
 
 # Write a function like this called 'main'
 def main(job_id, params):
     time.sleep(5)
-    print 'Anything printed here will end up in the output directory for job #%d' % job_id
+    print 'Anything printed here will end up in the \
+    output directory for job #%d' % job_id
     print params
     return branin(params['x'], params['y'])
+
 
 def get_validation_accuracies(job_id, params):
     print 'getting validation accs'
